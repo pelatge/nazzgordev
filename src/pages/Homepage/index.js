cac2d5a11f95d71fc254b6ebe65d894dbee2d4ae
pelatge <pelat.g29@gmail.com>
2022-02-07 15:05:42 +0700

    edit pages

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -3,7 +3,7 @@ import { React, Link } from "./../../libraries";
 const Homepage = () => {
   return (
     <div>
-      <h1>Selamat datang di Kabayan Coding</h1>
+      <h1>Selamat datang di Nazggordev</h1>
       <p>
         Klik <Link to="/about">disini</Link> ke halaman tentang saya
       </p>
